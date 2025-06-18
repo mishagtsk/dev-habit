069eb00c758f3d67efdb78afc515cc15f215f49f
Mykhailo Hutsalenko <m.hutsalenko@playwildforest.io>
2025-06-18 15:09:50 +0300

    Add tags controller

---
+++ DevHabit.Api/DTOs/Tags/TagDto.cs
@@ -0,0 +1,10 @@
+namespace DevHabit.Api.DTOs.Tags;
+
+public sealed record TagDto
+{
+    public required string Id { get; init; }
+    public required string Name { get; init; }
+    public string? Description { get; init; }
+    public required DateTime CreatedAtUtc { get; init; }
+    public DateTime? UpdatedAtUtc { get; init; }
+}
---
+++ DevHabit.Api/DTOs/Tags/TagsCollectionDto.cs
@@ -0,0 +1,6 @@
+namespace DevHabit.Api.DTOs.Tags;
+
+public sealed class TagsCollectionDto
+{
+    public List<TagDto> Data { get; set; }
+}
