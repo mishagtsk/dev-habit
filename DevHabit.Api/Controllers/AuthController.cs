10519def408cf1c580d0989211878c779b48a123
Mykhailo Hutsalenko <m.hutsalenko@playwildforest.io>
2025-06-21 01:04:26 +0300

    Add refresh token support

--- DevHabit.Api/Controllers/AuthController.cs
+++ DevHabit.Api/Controllers/AuthController.cs
@@ -3,11 +3,13 @@ using DevHabit.Api.DTOs.Auth;
 using DevHabit.Api.DTOs.Users;
 using DevHabit.Api.Entities;
 using DevHabit.Api.Services;
+using DevHabit.Api.Settings;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Options;
 
 namespace DevHabit.Api.Controllers;
 
@@ -18,8 +20,11 @@ public class AuthController(
     UserManager<IdentityUser> userManager,
     ApplicationDbContext applicationDbContext,
     ApplicationIdentityDbContext identityDbContext,
-    TokenProvider tokenProvider) : ControllerBase
+    TokenProvider tokenProvider,
+    IOptions<JwtAuthOptions> options) : ControllerBase
 {
+    private readonly JwtAuthOptions _jwtAuthOptions = options.Value;
+    
     [HttpPost("register")]
     public async Task<ActionResult<AccessTokensDto>> Register(RegisterUserDto registerUserDto)
     {
@@ -56,14 +61,26 @@ public class AuthController(
         
         await applicationDbContext.SaveChangesAsync();
         
-        await transaction.CommitAsync();
-
         var tokenRequest = new TokenRequest(identityUser.Id, identityUser.Email);
         
-        AccessTokensDto accessToken = tokenProvider.Create(tokenRequest);
+        AccessTokensDto accessTokens = tokenProvider.Create(tokenRequest);
+
+        var refreshToken = new RefreshToken
+        {
+            Id = Guid.CreateVersion7(),
+            UserId = user.Id,
+            Token = accessTokens.RefreshToken,
+            ExpiresAtUtc = DateTime.UtcNow.AddDays(_jwtAuthOptions.RefreshTokenExpirationDays),
+        };
+        
+        identityDbContext.RefreshTokens.Add(refreshToken);
+        
+        await identityDbContext.SaveChangesAsync();
+        
+        await transaction.CommitAsync();
         
         // return
-        return Ok(accessToken);
+        return Ok(accessTokens);
     }
 
     [HttpPost("login")]
@@ -79,6 +96,47 @@ public class AuthController(
         var tokenRequest = new TokenRequest(identityUser.Id, identityUser.Email!);
         
         AccessTokensDto accessTokens = tokenProvider.Create(tokenRequest);
+        
+        var refreshToken = new RefreshToken
+        {
+            Id = Guid.CreateVersion7(),
+            UserId = identityUser.Id,
+            Token = accessTokens.RefreshToken,
+            ExpiresAtUtc = DateTime.UtcNow.AddDays(_jwtAuthOptions.RefreshTokenExpirationDays),
+        };
+        
+        identityDbContext.RefreshTokens.Add(refreshToken);
+        
+        await identityDbContext.SaveChangesAsync();
+        
+        return Ok(accessTokens);
+    }
+
+    [HttpPost("refresh")]
+    public async Task<ActionResult<AccessTokensDto>> Refresh(RefreshTokenDto refreshTokenDto)
+    {
+        RefreshToken? refreshToken = await identityDbContext.RefreshTokens
+            .Include(t => t.User)
+            .FirstOrDefaultAsync(t => t.Token == refreshTokenDto.RefreshToken);
+
+        if (refreshToken == null)
+        {
+            return Unauthorized();
+        }
+
+        if (refreshToken.ExpiresAtUtc < DateTime.UtcNow)
+        {
+            return Unauthorized();
+        }
+        
+        var tokenRequest = new TokenRequest(refreshToken.User.Id, refreshToken.User.Email!);
+        
+        AccessTokensDto accessTokens = tokenProvider.Create(tokenRequest);
+        
+        refreshToken.Token = accessTokens.RefreshToken;
+        refreshToken.ExpiresAtUtc = DateTime.UtcNow.AddDays(_jwtAuthOptions.RefreshTokenExpirationDays);
+        
+        await identityDbContext.SaveChangesAsync();
         
         return Ok(accessTokens);
     }
--- DevHabit.Api/Services/TokenProvider.cs
+++ DevHabit.Api/Services/TokenProvider.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using DevHabit.Api.DTOs.Auth;
 using DevHabit.Api.Settings;
@@ -45,8 +46,10 @@ public sealed class TokenProvider(IOptions<JwtAuthOptions> options)
         return accessToken;
     }
     
-    private string GenerateRefreshToken()
+    private static string GenerateRefreshToken()
     {
-        return string.Empty;
+        byte[] randomBytes = RandomNumberGenerator.GetBytes(32);
+        
+        return Convert.ToBase64String(randomBytes);
     }
 }
