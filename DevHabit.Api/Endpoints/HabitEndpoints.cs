5053dab32a204e5410e225f48b83cce0f9e1d941
Mykhailo Hutsalenko <m.hutsalenko@playwildforest.io>
2025-07-20 20:35:34 +0300

    Minimal API - migrate PatchHabit endpoint

--- DevHabit.Api/Endpoints/HabitEndpoints.cs
+++ DevHabit.Api/Endpoints/HabitEndpoints.cs
@@ -8,9 +8,11 @@ using DevHabit.Api.Entities;
 using DevHabit.Api.Services;
 using DevHabit.Api.Services.Sorting;
 using FluentValidation;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
 
 namespace DevHabit.Api.Endpoints;
 
@@ -64,6 +66,15 @@ public static class HabitEndpoints
         
         group.MapPut("/{id}", UpdateHabit)
             .WithName(nameof(UpdateHabit))
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status401Unauthorized)
+            .Produces(StatusCodes.Status403Forbidden)
+            .Produces(StatusCodes.Status404NotFound)
+            .WithOpenApi();
+        
+        group.MapPatch("/{id}", PatchHabit)
+            .WithName(nameof(PatchHabit))
             .Produces(StatusCodes.Status204NoContent)
             .Produces(StatusCodes.Status400BadRequest)
             .Produces(StatusCodes.Status401Unauthorized)
@@ -371,6 +382,50 @@ public static class HabitEndpoints
         }
 
         habit.UpdateFromDto(updateHabitDto);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Results.NoContent();
+    }
+    
+    /// <summary>
+    /// Partially updates an existing habit using JSON Patch
+    /// </summary>
+    /// <returns>No content on success</returns>
+    private static async Task<IResult> PatchHabit(string id,
+        UserContext userContext,
+        HttpContext context,
+        ApplicationDbContext dbContext,
+        CancellationToken cancellationToken)
+    {
+        string? userId = await userContext.GetUserIdAsync(cancellationToken);
+
+        if (string.IsNullOrEmpty(userId))
+        {
+            return Results.Unauthorized();
+        }
+
+        Habit? habit =
+            await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id && h.UserId == userId, cancellationToken);
+
+        if (habit == null)
+        {
+            return Results.NotFound();
+        }
+
+        HabitDto habitDto = habit.ToDto();
+
+        using var streamReader = new StreamReader(context.Request.Body);
+        JsonPatchDocument<HabitDto> patchDocument =
+            JsonConvert.DeserializeObject<JsonPatchDocument<HabitDto>>(await streamReader.ReadToEndAsync(cancellationToken))!;
+        
+        patchDocument.ApplyTo(habitDto);
+
+        // Do manual validation of DTO
+
+        habit.Name = habitDto.Name;
+        habit.Description = habitDto.Description;
+        habit.UpdatedAtUtc = DateTime.UtcNow;
 
         await dbContext.SaveChangesAsync(cancellationToken);
 
